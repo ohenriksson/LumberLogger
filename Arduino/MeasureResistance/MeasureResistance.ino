dd974d5200cc3fdbbc9bfa9704bcb954cee67dc3
Oskar Henriksson <o.henriksson@gmail.com>
2020-03-29 12:38:58 +0200

    wrap resistance measuring into class ResistanceCollector

--- Arduino/MeasureResistance/MeasureResistance.ino
+++ Arduino/MeasureResistance/MeasureResistance.ino
@@ -1,97 +1,118 @@
 
-int led_pin = 13;
-int sensorPin = A0; //A0    // select the input pin for the potentiometer
-int enableA = A1; //A1
-int enableB = A2; //A2
+class ResistanceCollector{
+  private:
+    const int led_pin = 13;
+    const int sensorPin = A0; //A0    // select the input pin for the potentiometer
+    const int enableA = A1; //A1
+    const int enableB = A2; //A2
 
-int sensorValueA = 0;  // variable to store the value coming from the sensor
-int sensorValueB = 0;  // variable to store the value coming from the sensor
+    const float AnalogMax = 1024.000;
+    const float VoltMax = 3.300;
+    const float VoltApplied = 3.300;
+    
+    const int delay1 = 1000;
+    const int pauseDelay = 5000;
 
-float AnalogMax = 1024.000;
-float VoltMax = 3.300;
-float VoltNow = 0;
-float VoltApplied = 3.300;
+    const double resA = 1000000 * 3.3; //3.3M
+    const double resB = 1000000 * 220; //220M
+    
+  public:
+    float VoltNow = 0; // current sensor voltage
+    int sensorValueA = 0;  // variable to store the value coming from the sensor
+    int sensorValueB = 0;  // variable to store the value coming from the sensor
 
-double resA = 1000000 * 3.3; //3.3M
-double resB = 1000000 * 220; //220M
+    ResistanceCollector(){
+    }
+    
+    void SetupPins(){
+      pinMode(led_pin, OUTPUT);
+      pinMode(enableA, OUTPUT);
+      pinMode(enableB, OUTPUT);
+      
+      Serial.begin(9600);
+      while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB port only
+      }
+    }
 
-int delay1 = 1000;
-int pauseDelay = 5000;
+    float ConvertToVolt(float analogValue){
+      return analogValue * (VoltMax / AnalogMax);
+    }
+    
+    float VoltToOhm(float sensorVolt, float knownR){
+      if(VoltApplied-sensorVolt < 0.01) return 0; //error, prevent overflow
+      return knownR * sensorVolt / (VoltApplied - sensorVolt);
+    }
+
+    int ReadValueA(){
+      EnableA();
+      delay(delay1); 
+      sensorValueA = analogRead(sensorPin);
+      delay(delay1);
+      DisableAll(); 
+    }
+    
+    int ReadValueB(){
+      EnableB();
+      delay(delay1); 
+      sensorValueB = analogRead(sensorPin);
+      delay(delay1); 
+      DisableAll();
+    }
+    
+    void EnableA(){
+      digitalWrite(enableA, HIGH);
+      digitalWrite(enableB, LOW);
+    }
+    
+    void DisableAll(){
+      digitalWrite(enableA, LOW);
+      digitalWrite(enableB, LOW);
+    }
+    
+    void EnableB(){
+      digitalWrite(enableA, LOW);
+      digitalWrite(enableB, HIGH);
+    }
+
+  
+    
+    void ReadAndPrintValue(int knownR, String sensor){
+      Serial.print(sensor + " -- ");
+      if(sensor == "sensor A")
+      {
+        ReadValueA();
+        VoltNow = ConvertToVolt(sensorValueA);
+      }
+      else if(sensor == "sensor B")
+      {
+        ReadValueB();
+        VoltNow = ConvertToVolt(sensorValueB);
+      } 
+      
+      Serial.print(VoltNow);
+      Serial.print("V ");
+      Serial.print(VoltToOhm(VoltNow, knownR));
+      Serial.print("Ohm --- \n");
+    }
+
+    void Loop(){
+      ReadAndPrintValue(resA, "sensor A"); 
+      delay(pauseDelay);
+      ReadAndPrintValue(resB, "sensor B");
+      delay(pauseDelay); 
+    }
+};
+
+
+
+
+ResistanceCollector coll1 = ResistanceCollector();
 
 void setup() {
-  pinMode(led_pin, OUTPUT);
-  pinMode(enableA, OUTPUT);
-  pinMode(enableB, OUTPUT);
-  
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  coll1.SetupPins();
 }
 
 void loop() {
-  ReadAndPrintValue(resA, "sensor A"); 
-  delay(pauseDelay);
-  ReadAndPrintValue(resB, "sensor B");
-  delay(pauseDelay);
-}
-
-float ConvertToVolt(float analogValue){
-  return analogValue * (VoltMax / AnalogMax);
-}
-
-float VoltToOhm(float sensorVolt, float knownR){
-  if(VoltApplied-sensorVolt < 0.01) return 0; //error, prevent overflow
-  return knownR * sensorVolt / (VoltApplied - sensorVolt);
-}
-
-void ReadAndPrintValue(int knownR, String sensor){
-
-  Serial.print(sensor + " -- ");
-  if(sensor == "sensor A")
-  {
-    ReadValueA();
-    VoltNow = ConvertToVolt(sensorValueA);
-  }
-  else if(sensor == "sensor B")
-  {
-    ReadValueB();
-    VoltNow = ConvertToVolt(sensorValueB);
-  } 
-  
-  Serial.print(VoltNow);
-  Serial.print("V ");
-  Serial.print(VoltToOhm(VoltNow, knownR));
-  Serial.print("Ohm --- \n");
-}
-
-int ReadValueA(){
-  EnableA();
-  delay(delay1); 
-  sensorValueA = analogRead(sensorPin);
-  delay(delay1);
-  DisableAll(); 
-}
-
-int ReadValueB(){
-  EnableB();
-  delay(delay1); 
-  sensorValueB = analogRead(sensorPin);
-  delay(delay1); 
-  DisableAll();
-}
-
-void EnableA(){
-  digitalWrite(enableA, HIGH);
-  digitalWrite(enableB, LOW);
-}
-
-void DisableAll(){
-  digitalWrite(enableA, LOW);
-  digitalWrite(enableB, LOW);
-}
-
-void EnableB(){
-  digitalWrite(enableA, LOW);
-  digitalWrite(enableB, HIGH);
+  coll1.Loop();
 }
