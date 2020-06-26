7a56a170963996bba465e08af9babcc6bc95a827
Oskar Henriksson <o.henriksson@gmail.com>
2020-06-26 23:31:02 +0200

    avg 64 to firebase

--- Arduino/WiFiFirebase/wifiFirebase.ino
+++ Arduino/WiFiFirebase/wifiFirebase.ino
@@ -1,5 +1,6 @@
 int status = WL_IDLE_STATUS;
 double readArray1[5];
+double sumAvg1[64];
 double readArray2[5];
 double spruceArray[] = {22400 , 5890,  2140,  830, 365, 165, 83,  44,  25.1,  15.5,  9.8, 6.3, 4.27,  3.02,  2.14,  1.58,  1.17,  0.91,  0.71}; //7 -> 25
 
@@ -14,42 +15,42 @@ void setup() {
 
 void loop(){
 
-  averageOver(500);
-
+  averageOver64();
+  delay(1000 * 60);
 }
 
-void averageOver(int milliS){
+void averageOver64(){
   double avg = 0;
-  double sum = 0;
-  double sum2 = 0;
+  double avg2 = 0;
+  String jsonRead = "";
   
-  for(int i=0; i < milliS; i+=10){
+  for(int i=0; i < 64; i+=10){
+      double sum = 0;
+      double sum2 = 0;
     doNMeasurements(5);
-     for(int i=0; i<5; i++){
-       sum += readArray1[i]/5;
-       sum2 += readArray2[i]/5;
+     for(int j=0; j<5; j++){
+       sum += readArray1[j] / 5.0;
+       sum2 += readArray2[j] / 5.0;
     }
+    avg += sum/64;
+    avg2 += sum2/64;
   }
 
-  Serial.print(sum);
+
+  Serial.print(avg);
   Serial.print(" ");
-  Serial.print(sum2);
+  Serial.print(avg2);
   Serial.print("\n");
+
+  
+  jsonRead = "{\"R1\":" + String(avg, DEC) + "," + "\"R2\":" + String(avg2, DEC) + "}";
+  FirebaseSet(jsonRead);
 }
 
 double doNMeasurements(int nMeas){
-  double sum = 0;
-  double sum2 = 0;
-  
   for(int i=0; i<nMeas; i++){
     doReadingAndPost(i);
-  }
-  
-  for(int i=0; i<nMeas; i++){
-    sum += readArray1[i];
-    sum2 += readArray2[i];
-  }
-  
+  } 
 }
 
 void doReadingAndPost(int i){
@@ -59,12 +60,6 @@ void doReadingAndPost(int i){
   VoltageReading read2 = coll1.ReadSensor(SensorEnum::sensorB);
   readArray2[i]=read2.unkownR;
   
-  //String jsonRead2 = read2.ToJSON();
-  //Serial.println("json:" + jsonRead1);
-  //Serial.println("json:" + jsonRead2);
 
-  //Serial.println("Set values");
-  //FirebaseSet(jsonRead1);
-  //FirebaseSet(jsonRead2);
 
 }
